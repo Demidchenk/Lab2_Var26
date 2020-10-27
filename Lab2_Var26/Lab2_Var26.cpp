e971dcfd0f0c299573cb5aa74fb43c9482b7d9f1
Алексей <alexey.demidchenko@lll.kpi.ua>
2020-10-28 00:11:35 +0200

    Add project files.

---
+++ Lab2_Var26/Lab2_Var26.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+
+int main()
+{
+    double x, y;
+    std::cout << "Введіть координати точки\n";
+    std::cin >> x >> y;
+    bool res = false;
+    if (x * x + y * y > 4) res = false;
+    else if (x * y > 0)
+        if (abs(x) + abs(y) >= 2) res = true;
+        else res = false;
+    else if (abs(x) + abs(y) <= 2) res = true;
+    else res = false;
+    if (res == true) std::cout << "Точка належить заштрихованій частині";
+    else std::cout << "Точка не належить заштрихованій частині";
+}
