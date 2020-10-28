f1fbf62fb8c71130541175635881f94c7cacda64
Demidchenk <71965474+Demidchenk@users.noreply.github.com>
2020-10-28 11:25:17 +0200

    Update Lab2_Var26.cpp

--- Lab2_Var26/Lab2_Var26.cpp
+++ Lab2_Var26/Lab2_Var26.cpp
@@ -1,10 +1,11 @@
 ﻿#include <iostream>
+using namespace std;
 
 int main()
 {
     double x, y;
-    std::cout << "Введіть координати точки\n";
-    std::cin >> x >> y;
+    cout << "Введіть координати точки\n";
+    cin >> x >> y;
     bool res = false;
     if (x * x + y * y > 4) res = false;
     else if (x * y > 0)
@@ -12,6 +13,6 @@ int main()
         else res = false;
     else if (abs(x) + abs(y) <= 2) res = true;
     else res = false;
-    if (res == true) std::cout << "Точка належить заштрихованій частині";
-    else std::cout << "Точка не належить заштрихованій частині";
+    if (res == true) cout << "Точка належить заштрихованій частині";
+    else cout << "Точка не належить заштрихованій частині";
 }
